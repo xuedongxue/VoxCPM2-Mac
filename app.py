575a6216e4f93f469df8c9621adb214b81cf1d24
刘鑫 <liuxin@modelbest.cn>
2025-09-16 23:46:52 +0800

    reduce concurrency_limit

--- app.py
+++ app.py
@@ -499,7 +499,7 @@ def create_demo_interface(client: RayServeVoxCPMClient):
             outputs=[audio_output],
             show_progress=True,
             api_name="generate",
-            concurrency_limit=None,
+            concurrency_limit=5,
         )
         prompt_wav.change(fn=client.prompt_wav_recognition, inputs=[prompt_wav], outputs=[prompt_text])
         
